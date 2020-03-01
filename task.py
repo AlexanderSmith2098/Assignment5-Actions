11afdc5d6d5ba58db1599da8112111ac5e1877c8
AlexanderSmith2098 <smitale2@oregonstate.edu>
2020-03-01 11:30:04 -0800

    task1

--- task.py
+++ task.py
@@ -1,5 +1,6 @@
 def firstrun():
     return "success"
 
+
 def radius(rad):
     return 3.14 * rad * rad
--- tests.py
+++ tests.py
@@ -11,10 +11,11 @@ class TestCase(unittest.TestCase):
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
-    
+
     def testradius(self):
         expected = 3.14
         self.assertEqual(expected, task.radius(1))
+
 
 if __name__ == '__main__':
     unittest.main()
